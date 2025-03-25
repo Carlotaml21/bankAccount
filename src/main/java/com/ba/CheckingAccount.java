cbe9ff8d92c20c30060580c8919302597ae368bd
Carlotaml21 <carlotaml21@gmail.com>
2025-03-25 14:14:40 +0100

    feature: create SavingsAccount and add method (TDD:GREEN)

--- src/main/java/com/ba/CheckingAccount.java
+++
@@ -1,5 +0,0 @@
-package com.ba;
-
-public class CheckingAccount extends BankAccount{
-
-}
--- src/main/java/com/ba/SavingsAccount.java
+++ src/main/java/com/ba/SavingsAccount.java
@@ -1,4 +1,15 @@
 package com.ba;
 
 public class SavingsAccount extends BankAccount {
+
+    private boolean active;
+
+    public SavingsAccount(float initialBalance, float annualRate) {
+        super(initialBalance, annualRate);
+        this.active = initialBalance >=10000;
+    }
+
+    public boolean isActive(){
+        return active;
+    }
 }
