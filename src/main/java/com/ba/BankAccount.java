0c5959a655d1f20102c3d3f5b140f694f991a97e
Carlotaml21 <carlotaml21@gmail.com>
2025-03-25 18:32:59 +0100

    feat:add getter depositCount

--- src/main/java/com/ba/BankAccount.java
+++ src/main/java/com/ba/BankAccount.java
@@ -21,6 +21,9 @@ public class BankAccount {
     public float getAnnualRate() {
         return annualRate;
     }
+    public int getDepositCount() {
+        return depositCount;
+    }
 
     public int getWithdrawCount() {
         return withdrawCount;
