06560409952d176daa818f2f25c0fc29a60ba483
Carlotaml21 <carlotaml21@gmail.com>
2025-03-27 09:26:48 +0100

    feat:add deposit override (TDD:GREEN)

--- src/main/java/com/ba/CheckingAccount.java
+++ src/main/java/com/ba/CheckingAccount.java
@@ -1,29 +1,44 @@
 package com.ba;
 
-public class CheckingAccount  extends BankAccount{
+public class CheckingAccount  extends BankAccount {
     private float overdraft = 0f;
 
-    public CheckingAccount(float initialBalance, float annualRate){
+    public CheckingAccount(float initialBalance, float annualRate) {
         super(initialBalance, annualRate);
     }
 
-    public float getOverdraft(){
+    public float getOverdraft() {
         return overdraft;
     }
 
     @Override
-    public void withdraw(float amount){
+    public void withdraw(float amount) {
         float actualBalance = getBalance();
 
-        if(amount<= actualBalance) {
+        if (amount <= actualBalance) {
             super.withdraw(amount);
-        }else {
+        } else {
             float difference = amount - actualBalance;
 
             super.withdraw(actualBalance);
             overdraft += difference;
         }
-
     }
-}
+
+    @Override
+        public void deposit(float amount) {
+            if (overdraft > 0) {
+                if (amount >= overdraft) {
+                    float remainder = amount - overdraft;
+                    overdraft = 0;
+                    super.deposit(remainder); // lo que sobra va al saldo
+                } else {
+                    overdraft -= amount; // aún queda sobregiro
+                }
+            } else {
+                super.deposit(amount);
+            }
+        }
+    }
+
 
