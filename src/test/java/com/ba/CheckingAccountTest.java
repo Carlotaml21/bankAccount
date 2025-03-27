1ec1f95e48476fe7d121e111f3d2414c098d5569
Carlotaml21 <carlotaml21@gmail.com>
2025-03-27 09:07:56 +0100

    feat: fix addCheckingAccountTest (TDD:RED)

--- src/test/java/com/ba/CheckingAccountTest.java
+++ src/test/java/com/ba/CheckingAccountTest.java
@@ -1,12 +1,16 @@
 package com.ba;
+
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class CheckingAccountTest{
+
+    @Test
+        public void testOverdraft(){
     CheckingAccount account = new CheckingAccount (1000f, 005f);
     account.withdraw(1500f);
 
     assertEquals(0f, account.getBalance(), 0,001f);
     assertEquals(500f, account.getOverdraft(), 0.001f);
-
+    }
 }
