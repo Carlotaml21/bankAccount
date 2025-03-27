7b4f8d998245b13441f2b9e4d79fb4ec7f616a6f
Carlotaml21 <carlotaml21@gmail.com>
2025-03-27 09:34:20 +0100

    test:add testMonthlyStatementCheckingAccount() (TDD:RED)

--- src/test/java/com/ba/CheckingAccountTest.java
+++ src/test/java/com/ba/CheckingAccountTest.java
@@ -22,4 +22,15 @@ public class CheckingAccountTest{
         assertEquals(0f, account.getBalance(), 0.001f);
         assertEquals(200f, account.getOverdraft(), 0.001f);
     }
+
+    @Test public void testMonthlyStatementCheckingAccount(){
+        CheckingAccount account = new CheckingAccount (1000f, 0.05f);
+        account.setMonthlyFee(500f);
+
+        account.monthlyStatement();
+
+        float expected = (10000f - 500f) + ((10000f - 500f) * (0.12f/ 12));
+
+        assertEquals(expected, account.getBalance(), 0.001f);
+    }
 }
